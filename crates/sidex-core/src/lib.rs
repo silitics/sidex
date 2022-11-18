a4ce834fa6f03a4edea218233a3cbe68b67c908f
Maximilian Köhl <mail@koehlma.de>
2022-11-18 13:20:21 +0100

    chore: refactor diagnostics into their own crate

--- crates/sidex-core/src/lib.rs
+++ crates/sidex-core/src/lib.rs
@@ -1,9 +1,9 @@
 #![doc = include_str!("../README.md")]
 
+pub use sidex_diagnostics as diagnostics;
 pub use sidex_ir as ir;
 
 pub mod builtins;
 pub mod bundle;
-pub mod diagnostics;
 pub mod transformer;
 pub mod utils;
